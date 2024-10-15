c3bb3ed44d2504474836255fb5b0087b00d2ce1f
Javier Caceres <81489497+srnovus@users.noreply.github.com>
2024-10-15 17:46:01 -0600

    Update store.ts
    Signed-off-by: Javier Caceres <81489497+srnovus@users.noreply.github.com>

--- packages/client/src/store.ts
+++ packages/client/src/store.ts
@@ -250,7 +250,7 @@ export const defaultStore = markRaw(
 		},
 		showFixedPostForm: {
 			where: "device",
-			default: ture,
+			default: true,
 		},
 		enableInfiniteScroll: {
 			where: "device",
