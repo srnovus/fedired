ec836b37aadf761b0a267b897ebe17ae290e58b9
Javier Caceres <81489497+srnovus@users.noreply.github.com>
2024-10-15 22:24:37 -0600

    Create MkUserNameWithVerification.vue
    Signed-off-by: Javier Caceres <81489497+srnovus@users.noreply.github.com>

---
+++ packages/client/src/components/MkUserNameWithVerification.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="user-name">
+		<MkUserName :user="user" :nowrap="true" />
+		<span v-if="user.isAdmin && user.username === 'srnovus'" v-tooltip.noDelay="'Verificado'" style="color: var(--badge); margin-left: 4px;">
+			<i :class="icon('ph-bold ph-seal-check')" style="font-size: 1.2em;"></i> <!-- Ícono de verificación para administrador -->
+		</span>
+		<span v-if="user.isModerator && user.username === 'fedired'" v-tooltip.noDelay="'Verificado'" style="color: var(--badge); margin-left: 4px;">
+			<i :class="icon('ph-bold ph-seal-check')" style="font-size: 1.2em;"></i> <!-- Ícono de verificación para moderador -->
+		</span>
+	</div>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+	user: entities.UserDetailed;
+}>();
+</script>
