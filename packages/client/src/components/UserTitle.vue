55b432f7e7f49a840379a8f3daff8fd122355951
Javier Caceres <81489497+srnovus@users.noreply.github.com>
2024-10-17 20:45:15 -0600

    Create UserTitle.vue
    Signed-off-by: Javier Caceres <81489497+srnovus@users.noreply.github.com>

---
+++ packages/client/src/components/UserTitle.vue
@@ -0,0 +1,20 @@
+   <template>
+       <div>
+           <span>{{ user.name }} (@{{ user.username }})</span>
+           <span v-if="isAdmin" v-tooltip.noDelay="'Verificado'" style="color: var(--badge); margin-left: 4px;">
+               <i :class="icon('ph-bold ph-seal-check')"></i>
+           </span>
+           <span v-if="isModerator" v-tooltip.noDelay="'Verificado'" style="color: var(--badge); margin-left: 4px;">
+               <i :class="icon('ph-bold ph-seal-check')"></i>
+           </span>
+       </div>
+   </template>
+
+   <script lang="ts" setup>
+   defineProps<{
+       user: entities.UserDetailed;
+   }>();
+
+   const isAdmin = computed(() => user.isAdmin && user.username === 'srnovus');
+   const isModerator = computed(() => user.isModerator && user.username === 'fedired');
+   </script>
