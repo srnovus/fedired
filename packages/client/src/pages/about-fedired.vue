5d814055d89c1d7569870de48d08abdd8785bd74
Javier Caceres <81489497+srnovus@users.noreply.github.com>
2024-12-02 12:54:54 -0600

    Update about-fedired.vue
    change code 
    
    Signed-off-by: Javier Caceres <81489497+srnovus@users.noreply.github.com>

--- packages/client/src/pages/about-fedired.vue
+++ packages/client/src/pages/about-fedired.vue
@@ -1,110 +1,107 @@
 <template>
-	<MkStickyContainer>
-		<template #header
-			><MkPageHeader :actions="headerActions" :tabs="headerTabs"
-		/></template>
-		<div style="overflow: clip">
-			<MkSpacer :content-max="600" :margin-min="20">
-				<div class="_formRoot znqjceqz">
-					<div id="debug"></div>
-					<div
-						ref="containerEl"
-						v-panel
-						class="_formBlock about"
-						:class="{ playing: easterEggEngine != null }"
-					>
-						<img
-							src="/client-assets/about-icon.png"
-							alt=""
-							class="icon"
-							draggable="false"
-							@load="iconLoaded"
-							@click="gravity"
-						/>
-						<div class="misskey">Fedired</div>
-						<div class="version">v{{ version }}</div>
-						<span
-							v-for="emoji in easterEggEmojis"
-							:key="emoji.id"
-							class="emoji"
-							:data-physics-x="emoji.left"
-							:data-physics-y="emoji.top"
-							:class="{
-								_physics_circle_: !emoji.emoji.startsWith(':'),
-							}"
-						>
-							<MkEmoji
-								class="emoji"
-								:emoji="emoji.emoji"
-								:custom-emojis="instanceEmojis"
-								:is-reaction="false"
-								:normal="true"
-								:no-style="true"
-							/>
-						</span>
-					</div>
-					<div class="_formBlock" style="text-align: center">
-						{{ i18n.ts._aboutFedired.about }}<br /><a
-							href="https://help.fedired.com/project/fedired.html"
-							target="_blank"
-							class="_link"
-							>{{ i18n.ts.learnMore }}</a
-						>
-					</div>
-					<div class="_formBlock" style="text-align: center">
-						<MkButton primary rounded inline @click="iLoveMisskey"
-							>I <Mfm text="$[jelly ❤]" /> #Fedired</MkButton
-						>
-					</div>
-					<FormSection>
-						<div class="_formLinks">
-							<FormLink
-								to="https://github.com/fedired-dev/fedired/"
-								external
-							>
-								<template #icon
-									><i :class="icon('ph-code')"></i
-								></template>
-								{{ i18n.ts._aboutFedired.source }}
-								<template #suffix>Codigo Fuente</template>
-							</FormLink>
-							<FormLink
-								to="https://patreon.com/fedired/"
-								external
-							>
-								<template #icon>
-									<i :class="icon('ph-heart')"></i> 
-									</template>
-								Donar
-								<template #suffix>Donation</template>
-							</FormLink>
-														<FormLink
-								to="https://help.fedired.com/legal/licencias.html"
-								external
-							>
-								<template #icon>
-									<i :class="icon('ph-gavel')"></i> 
-									</template>
-								Legal
-								<template #suffix>Licencias</template>
-							</FormLink>
-						</div>
-					</FormSection>
-					<FormSection>
-						<template #label>Hecho por</template>
-						<div class="contributors">
-							<div class="contributor" v-for="contributor in contributors" :key="contributor.username">
-								<a :href="contributor.link" target="_blank" class="_contributor">
-									<img :src="contributor.avatar" class="contributorAvatar" />
-									<span class="contributorUsername">{{ contributor.username }}</span>
-								</a>
-							</div>
-						</div>
-					</FormSection>
-				</div>
-			</MkSpacer>
-		</div>
-	</MkStickyContainer>
+  <MkStickyContainer>
+    <template #header>
+      <MkPageHeader :actions="headerActions" :tabs="headerTabs"/>
+    </template>
+    <div style="overflow: clip">
+      <MkSpacer :content-max="600" :margin-min="20">
+        <div class="_formRoot znqjceqz">
+          <div id="debug"></div>
+          <div
+            ref="containerEl"
+            v-panel
+            class="_formBlock about"
+            :class="{ playing: easterEggEngine != null }"
+          >
+            <img
+              src="/client-assets/about-icon.png"
+              alt=""
+              class="icon"
+              draggable="false"
+              @load="iconLoaded"
+              @click="gravity"
+            />
+            <div class="misskey">Fedired</div>
+            <div class="version">v{{ version }}</div>
+            <span
+              v-for="emoji in easterEggEmojis"
+              :key="emoji.id"
+              class="emoji"
+              :data-physics-x="emoji.left"
+              :data-physics-y="emoji.top"
+              :class="{
+                _physics_circle_: !emoji.emoji.startsWith(':'),
+              }"
+            >
+              <MkEmoji
+                class="emoji"
+                :emoji="emoji.emoji"
+                :custom-emojis="instanceEmojis"
+                :is-reaction="false"
+                :normal="true"
+                :no-style="true"
+              />
+            </span>
+          </div>
+          <div class="_formBlock" style="text-align: center">
+            {{ i18n.ts._aboutFedired.about }}<br />
+            <a
+              href="https://help.fedired.com/project/fedired.html"
+              target="_blank"
+              class="_link"
+            >
+              {{ i18n.ts.learnMore }}
+            </a>
+          </div>
+          <div class="_formBlock" style="text-align: center">
+            <MkButton primary rounded inline @click="iLoveMisskey">
+              I <Mfm text="$[jelly ❤]" /> #Fedired
+            </MkButton>
+          </div>
+          <FormSection>
+            <div class="_formLinks">
+              <FormLink to="https://github.com/fedired-dev/fedired/" external>
+                <template #icon>
+                  <i :class="icon('ph-code')"></i>
+                </template>
+                {{ i18n.ts._aboutFedired.source }}
+                <template #suffix>Codigo Fuente</template>
+              </FormLink>
+              <FormLink to="https://patreon.com/fedired/" external>
+                <template #icon>
+                  <i :class="icon('ph-heart')"></i>
+                </template>
+                Donar
+                <template #suffix>Donation</template>
+              </FormLink>
+              <FormLink to="https://help.fedired.com/legal/licencias.html" external>
+                <template #icon>
+                  <i :class="icon('ph-gavel')"></i>
+                </template>
+                Legal
+                <template #suffix>Licencias</template>
+              </FormLink>
+            </div>
+          </FormSection>
+          <FormSection>
+            <template #label>Hecho por</template>
+            <div class="contributors">
+              <div
+                class="contributor"
+                v-for="contributor in contributors"
+                :key="contributor.username"
+              >
+                <a :href="contributor.link" target="_blank" class="_contributor">
+                  <img :src="contributor.avatar" class="contributorAvatar" />
+                  <span class="contributorUsername">{{ contributor.username }}</span>
+                </a>
+              </div>
+            </div>
+          </FormSection>
+        </div>
+      </MkSpacer>
+    </div>
+  </MkStickyContainer>
 </template>
 
 <script lang="ts" setup>
@@ -120,183 +117,193 @@ import * as os from "@/os";
 import { definePageMetadata } from "@/scripts/page-metadata";
 import icon from "@/scripts/icon";
 import { getInstanceInfo } from "@/instance";
+
 let easterEggReady = false;
 const easterEggEmojis = ref([]);
 const easterEggEngine = ref(null);
 const containerEl = ref<HTMLElement>();
 const instanceEmojis = getInstanceInfo().emojis;
+
 function iconLoaded() {
-	const emojis =
-		defaultStore.state.reactions.length > 0
-			? defaultStore.state.reactions
-			: defaultReactions;
-	const containerWidth = containerEl.value?.offsetWidth;
-	for (let i = 0; i < 32; i++) {
-		easterEggEmojis.value.push({
-			id: i.toString(),
-			top: -(128 + Math.random() * 256),
-			left: Math.random() * containerWidth,
-			emoji: emojis[Math.floor(Math.random() * emojis.length)],
-		});
-	}
-	nextTick(() => {
-		easterEggReady = true;
-	});
+  const emojis =
+    defaultStore.state.reactions.length > 0
+      ? defaultStore.state.reactions
+      : defaultReactions;
+  const containerWidth = containerEl.value?.offsetWidth;
+  for (let i = 0; i < 32; i++) {
+    easterEggEmojis.value.push({
+      id: i.toString(),
+      top: -(128 + Math.random() * 256),
+      left: Math.random() * containerWidth,
+      emoji: emojis[Math.floor(Math.random() * emojis.length)],
+    });
+  }
+  nextTick(() => {
+    easterEggReady = true;
+  });
 }
+
 function gravity() {
-	if (!easterEggReady) return;
-	easterEggReady = false;
-	easterEggEngine.value = physics(containerEl.value);
+  if (!easterEggReady) return;
+  easterEggReady = false;
+  easterEggEngine.value = physics(containerEl.value);
 }
+
 function iLoveMisskey() {
-	os.post({
-		initialText: "I $[jelly ❤] #Fedired",
-		instant: true,
-	});
+  os.post({
+    initialText: "I $[jelly ❤] #Fedired",
+    instant: true,
+  });
 }
+
 onBeforeUnmount(() => {
-	if (easterEggEngine.value) {
-		easterEggEngine.value.stop();
-	}
+  if (easterEggEngine.value) {
+    easterEggEngine.value.stop();
+  }
 });
+
 const headerActions = computed(() => []);
 const headerTabs = computed(() => []);
 definePageMetadata({
-	title: i18n.ts.aboutFedired,
-	icon: null,
+  title: i18n.ts.aboutFedired,
+  icon: null,
 });
+
 const contributors = [
-	{ username: '@srnovus', link: 'https://fedired.com/@srnovus', avatar: 'https://avatars.githubusercontent.com/u/81489497?v=4' },
-	];
+  {
+    username: '@srnovus',
+    link: 'https://fedired.com/@srnovus',
+    avatar: 'https://avatars.githubusercontent.com/u/81489497?v=4',
+  },
+];
 </script>
 
 <style lang="scss" scoped>
 .znqjceqz {
-	> .about {
-		position: relative;
-		text-align: center;
-		padding: 16px;
-		border-radius: var(--radius);
-		> .icon {
-			display: block;
-			width: 80px; // 
-			margin: 0 auto;
-			border-radius: 16px;
-			position: relative;
-			z-index: 1;
-		}
-		> .misskey {
-			margin: 0.75em auto 0 auto;
-			width: max-content;
-			position: relative;
-			z-index: 1;
-		}
-		> .version {
-			margin: 0 auto;
-			width: max-content;
-			opacity: 0.5;
-			position: relative;
-			z-index: 1;
-		}
-		> .emoji {
-			position: absolute;
-			z-index: 1;
-			top: 0;
-			left: 0;
-			visibility: hidden;
-			> .emoji {
-				pointer-events: none;
-				font-size: 24px;
-				width: 24px;
-			}
-		}
-		&.playing {
-			&,
-			* {
-				user-select: none;
-			}
-			* {
-				will-change: transform;
-			}
-			> .emoji {
-				visibility: visible;
-			}
-		}
-	}
+  > .about {
+    position: relative;
+    text-align: center;
+    padding: 16px;
+    border-radius: var(--radius);
+
+    > .icon {
+      display: block;
+      width: 80px;
+      margin: 0 auto;
+      border-radius: 16px;
+      position: relative;
+      z-index: 1;
+    }
+
+    > .misskey {
+      margin: 0.75em auto 0 auto;
+      width: max-content;
+      position: relative;
+      z-index: 1;
+    }
+
+    > .version {
+      margin: 0 auto;
+      width: max-content;
+      opacity: 0.5;
+      position: relative;
+      z-index: 1;
+    }
+
+    > .emoji {
+      position: absolute;
+      z-index: 1;
+      top: 0;
+      left: 0;
+      visibility: hidden;
+
+      > .emoji {
+        pointer-events: none;
+        font-size: 24px;
+        width: 24px;
+      }
+    }
+
+    &.playing {
+      &, * {
+        user-select: none;
+      }
+
+      * {
+        will-change: transform;
+      }
+
+      > .emoji {
+        visibility: visible;
+      }
+    }
+  }
 }
-// Estilos para la sección "Hecho por"
+
 ._formLinks {
-	display: flex;
-	flex-direction: column; // Cambia a fila si prefieres
-	align-items: center; // Centra los elementos
-	margin-top: 16px; // Espaciado superior
-	a {
-		display: flex;
-		align-items: center; // Alinea verticalmente el contenido
-		text-decoration: none; // Elimina el subrayado
-		color: inherit; // Hereda el color del texto
-		background: var(--MI_THEME-buttonBg); // Fondo del cuadro
-		border-radius: 8px; // Bordes redondeados
-		padding: 12px; // Espaciado interno
-		margin: 8px 0; // Espaciado entre contribuyentes
-		width: 100%; // Ancho completo
-		box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); // Sombra para el efecto de elevación
-		transition: transform 0.2s, box-shadow 0.2s; // Transición suave
-		&:hover {
-			transform: translateY(-2px); // Efecto de elevación al pasar el mouse
-			box-shadow: 0 6px 12px rgba(0, 0, 0, 0.2); // Sombra más intensa al pasar el mouse
-		}
-		img {
-			width: 50px; // Ajusta el tamaño de la imagen
-			height: 50px; // Ajusta el tamaño de la imagen
-			border-radius: 50%; // Hace que la imagen sea circular
-			margin-right: 12px; // Espaciado a la derecha de la imagen
-		}
-		span {
-			font-size: 18px; // Tamaño de fuente para el nombre de usuario
-			font-weight: bold; // Negrita para el nombre de usuario
-			color: var(--MI_THEME-textColor); // Color del texto
-		}
-	}
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 16px;
+  
+  a {
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    color: inherit;
+    background: var(--MI_THEME-buttonBg);
+    border-radius: 8px;
+    padding: 12px;
+    margin: 8px 0;
+    width: 100%;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    transition: transform 0.2s, box-shadow 0.2s;
+    
+    &:hover {
+      transform: translateY(-2px);
+      box-shadow: 0 6px 12px rgba(0, 0, 0, 0.2);
+    }
+
+    img {
+      width: 50px;
+      height: 50px;
+      border-radius: 50%;
+      margin-right: 12px;
+    }
+
+    span {
+      font-size: 18px;
+      font-weight: bold;
+      color: var(--MI_THEME-textColor);
+    }
+  }
 }
+
 .contributors {
-	display: grid;
-	grid-template-columns: repeat(auto-fill, minmax(150px, 1fr)); // Cuadrícula responsiva
-	gap: 16px; // Espaciado entre los cuadros
-	.contributor {
-		display: flex;
-		align-items: center;
-		padding: 12px;
-		background: var(--MI_THEME-buttonBg); // Fondo del cuadro
-		border-radius: 8px; // Bordes redondeados
-		box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); // Sombra para el efecto de elevación
-		transition: background 0.3s; // Transición suave
-		&:hover {
-			background: var(--MI_THEME-buttonHoverBg); // Cambio de fondo al pasar el mouse
-		}
-		a {
-			display: flex;
-			align-items: center;
-			text-decoration: none; // Elimina el subrayado
-			color: inherit; // Hereda el color del texto
-			img {
-				width: 40px; // Ajusta el tamaño de la imagen
-				height: 40px; // Ajusta el tamaño de la imagen
-				border-radius: 50%; // Hace que la imagen sea circular
-				margin-right: 8px; // Espaciado a la derecha de la imagen
-			}
-			span {
-				font-size: 16px; // Tamaño de fuente para el nombre de usuario
-				font-weight: bold; // Negrita para el nombre de usuario
-			}
-		}
-	}
-}
-.contributorAvatar {
-	width: 30px;
-	border-radius: 100%;
-}
-.contributorUsername {
-	margin-left: 12px;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+  gap: 16px;
+
+  .contributor {
+    display: flex;
+    align-items: center;
+    padding: 12px;
+    background: var(--MI_THEME-buttonBg);
+    border-radius: 8px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+
+    .contributorAvatar {
+      width: 50px;
+      height: 50px;
+      border-radius: 50%;
+      margin-right: 12px;
+    }
+
+    .contributorUsername {
+      font-size: 18px;
+      font-weight: bold;
+      color: var(--MI_THEME-textColor);
+    }
+  }
 }
 </style>
